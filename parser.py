386fb6ab5449ca037b66a5ed5139e88f22ce4d02
uhapko <77666882+uhapko@users.noreply.github.com>
2021-11-04 19:00:05 +0100

    Add files via upload

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ Created on Mon Nov  1 13:51:13 2021
 @author: uha18
 """
 
-#from math import sqrt
+import numpy as np
 #def main():
 #negacids = ["ASP", "GLU"]
 #posacids = ["LYS", "ARG", "HIS"]
@@ -23,35 +23,39 @@ with open(path, 'r') as pdbfile:
     lig_dict = dict() # creates the dictionary for ligands
     negacids = ["ASP", "GLU"] # creates and populates the list of codes of aminoacids with negatively charged side chains at physiological pH
     posacids = ["LYS", "ARG", "HIS"] # creates and populates the list of codes of aminoacids with positively charged side chains at physiological pH
-    pos_count = 0
-    neg_count = 0
-    hetero_count = 0 
-    sequence = []
-    
-    for line in pdbfile:
+    pos_count = 0 #counting positively charged residues
+    neg_count = 0 #counting negatively charged residues 
+    sequence = []    
+    for line in pdbfile: #reading the .pdb file line-by-line
         strcont = line.split() #splitting the line into a list of strings  
-        if strcont[0] == "ATOM":
+        if strcont[0] == "ATOM": #finding the part of the text related to the protein itself
             atom_type = strcont[2]
-            chain_id = strcont[4]
-            element_type = strcont[11]
-            if element_type in element_dict:
+            #chain_id = strcont[4]
+            element_type = strcont[11] #last column is the element type, we take it from there
+            if element_type in element_dict: #now we are populating the element dictionary
                 element_dict[element_type] += 1
             else:
                 element_dict[element_type] = 1
-            if atom_type == "CA":
-                aminoacid_code = strcont[3]
-                if aminoacid_code in aminoacid_dict:
+            if atom_type == "CA": #finding the alpha-carbons of the protein backbone
+                aminoacid_code = strcont[3] #extracting the aminoacid code
+                if aminoacid_code in aminoacid_dict: #now we are populating the aminoacid dictionary
                     aminoacid_dict[aminoacid_code] += 1
                 else:
                     aminoacid_dict[aminoacid_code] = 1
-                if aminoacid_code in negacids:
+                if aminoacid_code in negacids: #checking if the acid belongs to the set of negatively charged ones
                     neg_count += 1
-                elif aminoacid_code in posacids:
+                elif aminoacid_code in posacids: #checking if the acid belongs to the set of positively charged ones
                     pos_count +=1
-                sequence.append(aminoacid_code)
+                sequence.append(aminoacid_code) #assembling the sequence of the protein
                 #calpha_coord = (strcont[6], strcont[7], strcont[8])
-            elif strcont[0] == "HETATM" and strcont[3] != "HOH":
-                pass               
+        elif strcont[0] == "HETATM" and strcont[3] != "HOH": #finding the part of the text pertaining heteroatoms, but not waters => ligands
+            lig_id = strcont[3]
+            chain_id = strcont[4]
+            lig_glob_id = lig_id + "-" + chain_id # basically, in the 1fcn.pdb we are dealing with a dimeric protein with the same ligand bound to the chains A and B => we have two ligands, which are the same. That's why I decided ot create code the ligands in the following way: LIGAND_ID-CHAIN_ID
+            if lig_glob_id in lig_dict: #ligand dictionary is w
+                lig_dict[lig_glob_id] += 1
+            else:
+                lig_dict[lig_glob_id] = 1
     #print(sequence)
     print("Amino acid composition:")
     for keyaa in list(aminoacid_dict.keys()):
@@ -61,5 +65,7 @@ with open(path, 'r') as pdbfile:
         print(keyel, element_dict[keyel])
     print("Total number of positively charged residues:", pos_count)
     print("Total number of negatively charged residues:", neg_count)
-    #print ("Total number of ligands:", len(list(lig_set())))
-        
+    print('Number of ligands found:', str(len(set(lig_dict))))
+    #print('Ligand identifier'+"-"+"Chain identifier")
+    for keylig in list(lig_dict.keys()):
+        print(keylig)
